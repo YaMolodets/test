f0b7f880fd8bef21ac6dfe37cab6b6ec573569dc
gavnakoder <gavnakoder@sosiBiby.com>
2022-09-13 14:05:00 +0300

    complete Event class

---
+++ Lira/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lira
+{
+    public class Event
+    {
+        public string TypeOfInstitution { get; set; }
+        public string TypeOfIvent { get; set; }
+        public DateTime TimeEvent { get; set; }
+        public int CountTicket { get; set; }
+        public int PriceTicket { get; set; }
+    }
+}
--- Lira/Windows/AddEvent.xaml.cs
+++ Lira/Windows/AddEvent.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lira
+namespace Lira.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddEvent.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddEvent : Window
     {
-        public static List<Event> events = new List<Event>();
-        public MainWindow()
+        public AddEvent()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddEvent
         }
     }
 }
